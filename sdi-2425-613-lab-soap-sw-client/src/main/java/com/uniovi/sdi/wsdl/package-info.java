875d12fec3e2275010878d74ed91c2e27a8023db
Alejandro Rivada Rodríguez <UO295528@uniovi.es>
2025-04-07 19:27:52 +0200

    SDI-2425-613-SW-11.1- Servicio SOAP Cliente.

---
+++ sdi-2425-613-lab-soap-sw-client/src/main/java/com/uniovi/sdi/wsdl/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este archivo ha sido generado por Eclipse Implementation of JAXB v2.3.7 
+// Visite https://eclipse-ee4j.github.io/jaxb-ri 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2025.04.07 a las 06:51:52 PM CEST 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://uniovi.com/sdi/soap/ws", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.uniovi.sdi.wsdl;
